79c95db2a27f751da9bc8e7d3ab287cbb3a7e476
miguelfilpi <miguelfilpi@hotmail.com>
2022-10-24 23:55:45 -0300

    Extend The Facade Layer

---
+++ bin/custom/concerttours/src/concerttours/facades/TourFacade.java
@@ -0,0 +1,10 @@
+// Hybris123SnippetStart concerttours.facades.TourFacade
+package concerttours.facades;
+import concerttours.data.TourData;
+ 
+public interface TourFacade
+{
+    TourData getTourDetails(final String tourId);
+}
+//Hybris123SnippetEnd
+
